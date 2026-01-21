31ff01ac399386adbcd6e1bd375112ebefc7ce48
YashManoharChaudhari <yash-manohar.chaudhari@iu-study.org>
2026-01-21 15:50:44 +0100

    Updated frontend API base URL for production

--- AutoML_Pipeline_Builder/frontend/src/api.js
+++ AutoML_Pipeline_Builder/frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// FORCE local backend
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://automlpipelinebuilder.onrender.com",
 });
 
 export const uploadDataset = async (file) => {
